ac64960316e2ca90fe43039bcc466202f75a1b08
Emmanuel M <emmanuelbendavid@gmail.com>
2019-04-29 16:40:38 -0300

    Added debug activity with suitable EditText

---
+++ app/src/debug/java/rkr/simplekeyboard/debug/DebugActivity.java
@@ -0,0 +1,15 @@
+package rkr.simplekeyboard.debug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rkr.simplekeyboard.inputmethod.R;
+
+public class DebugActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_debug);
+	}
+}
