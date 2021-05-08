175757728b2f75e736c89c4b95ff498a91f344df
wittmane <ewittman@protonmail.com>
2021-05-08 14:23:32 -0500

    fix the language on spacebar when multiple locales are enabled that use the same language and layout

--- app/src/main/java/rkr/simplekeyboard/inputmethod/latin/utils/LanguageOnSpacebarUtils.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/latin/utils/LanguageOnSpacebarUtils.java
@@ -18,10 +18,10 @@ package rkr.simplekeyboard.inputmethod.latin.utils;
 
 import android.view.inputmethod.InputMethodSubtype;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
+import rkr.simplekeyboard.inputmethod.latin.RichInputMethodManager;
 import rkr.simplekeyboard.inputmethod.latin.RichInputMethodSubtype;
 
 /**
@@ -32,19 +32,12 @@ public final class LanguageOnSpacebarUtils {
     public static final int FORMAT_TYPE_LANGUAGE_ONLY = 1;
     public static final int FORMAT_TYPE_FULL_LOCALE = 2;
 
-    private static List<InputMethodSubtype> sEnabledSubtypes = Collections.emptyList();
-    private static boolean sIsSystemLanguageSameAsInputLanguage;
-
     private LanguageOnSpacebarUtils() {
         // This utility class is not publicly instantiable.
     }
 
     public static int getLanguageOnSpacebarFormatType(
             final RichInputMethodSubtype subtype) {
-        // Only this subtype is enabled and equals to the system locale.
-        if (sEnabledSubtypes.size() < 2 && sIsSystemLanguageSameAsInputLanguage) {
-            return FORMAT_TYPE_NONE;
-        }
         final Locale locale = subtype.getLocale();
         if (locale == null) {
             return FORMAT_TYPE_NONE;
@@ -52,7 +45,9 @@ public final class LanguageOnSpacebarUtils {
         final String keyboardLanguage = locale.getLanguage();
         final String keyboardLayout = subtype.getKeyboardLayoutSetName();
         int sameLanguageAndLayoutCount = 0;
-        for (final InputMethodSubtype ims : sEnabledSubtypes) {
+        final List<InputMethodSubtype> enabledSubtypes =
+                RichInputMethodManager.getInstance().getMyEnabledInputMethodSubtypeList(true);
+        for (final InputMethodSubtype ims : enabledSubtypes) {
             final String language = SubtypeLocaleUtils.getSubtypeLocale(ims).getLanguage();
             if (keyboardLanguage.equals(language) && keyboardLayout.equals(
                     SubtypeLocaleUtils.getKeyboardLayoutSetName(ims))) {
@@ -63,25 +58,5 @@ public final class LanguageOnSpacebarUtils {
         // locale and keyboard layout. Otherwise displaying language name is enough.
         return sameLanguageAndLayoutCount > 1 ? FORMAT_TYPE_FULL_LOCALE
                 : FORMAT_TYPE_LANGUAGE_ONLY;
-    }
-
-    public static void setEnabledSubtypes(final List<InputMethodSubtype> enabledSubtypes) {
-        sEnabledSubtypes = enabledSubtypes;
-    }
-
-    public static void onSubtypeChanged(final RichInputMethodSubtype subtype,
-           final boolean implicitlyEnabledSubtype, final Locale systemLocale) {
-        final Locale newLocale = subtype.getLocale();
-        if (systemLocale.equals(newLocale)) {
-            sIsSystemLanguageSameAsInputLanguage = true;
-            return;
-        }
-        if (!systemLocale.getLanguage().equals(newLocale.getLanguage())) {
-            sIsSystemLanguageSameAsInputLanguage = false;
-            return;
-        }
-        // If the subtype is enabled explicitly, the language name should be displayed even when
-        // the keyboard language and the system language are equal.
-        sIsSystemLanguageSameAsInputLanguage = implicitlyEnabledSubtype;
     }
 }
