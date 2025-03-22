61e8a66c913c591ee9178e450a822351d6990b21
Raimondas Rimkus <3480123+rkkr@users.noreply.github.com>
2025-03-22 08:56:30 +0200

    not used

--- app/src/main/java/rkr/simplekeyboard/inputmethod/latin/LatinIME.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/latin/LatinIME.java
@@ -37,7 +37,6 @@ import android.util.Printer;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowInsets;
