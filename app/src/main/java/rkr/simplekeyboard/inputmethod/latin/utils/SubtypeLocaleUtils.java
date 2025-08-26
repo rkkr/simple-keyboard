050ccfea6c48c647795bcd147c569b1ed7bcd15a
Shunnuo <>
2025-08-26 11:21:08 +0100

    Update SubtypeLocaleUtils.java

--- app/src/main/java/rkr/simplekeyboard/inputmethod/latin/utils/SubtypeLocaleUtils.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/latin/utils/SubtypeLocaleUtils.java
@@ -387,14 +387,11 @@ public final class SubtypeLocaleUtils {
                 case LOCALE_MALAY_MALAYSIA:
                 case LOCALE_DUTCH:
                 case LOCALE_POLISH:
-                    break;									
-                case LOCALE_PORTUGUESE_BRAZIL:				
-                    addLayout(LAYOUT_HCESAR, R.string.subtype_hcesar);					
-                    addGenericLayouts();
-                    break;					
+                    break;	
+                case LOCALE_PORTUGUESE_BRAZIL:
                 case LOCALE_PORTUGUESE_PORTUGAL:
                     addLayout(LAYOUT_HCESAR, R.string.subtype_hcesar);
-                    addGenericLayouts();
+                    addGenericLayouts();              
                     break;					
                 case LOCALE_ROMANIAN:
                 case LOCALE_SLOVAK:
